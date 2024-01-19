51ceb0a646b16b9c53cb1bad47cd7f64c00b99c2
Cindy Chen <xchen@lcs.on.ca>
2024-01-19 08:41:57 -0500

    Initial Commit

---
+++ TradingCards/TradingCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TradingCardsApp.swift
+//  TradingCards
+//
+//  Created by Cindy Chen on 2024-01-19.
+//
+
+import SwiftUI
+
+@main
+struct TradingCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
